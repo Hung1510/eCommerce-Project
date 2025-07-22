d73c3144c29244e91418839d597450df12aa0cb7
Phạm Trần Gia Hưng <giahung1510200519@gmail.com>
2025-07-23 02:52:53 +0700

    initial commit

---
+++ src/test/java/com/personalProject/basicshop/BasicShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalProject.basicshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
