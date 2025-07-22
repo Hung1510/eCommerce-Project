bb4c64ef3200de0455be46448316b3be63be4ddb
Phạm Trần Gia Hưng <giahung1510200519@gmail.com>
2025-07-23 03:07:58 +0700

    create classes

---
+++ src/main/java/com/personalProject/basicshop/model/Category.java
@@ -0,0 +1,9 @@
+package com.personalProject.basicshop.model;
+
+import java.util.List;
+
+public class Category {
+    private Long id;
+    private String name;
+    private List<Product> products;
+}
---
+++ src/main/java/com/personalProject/basicshop/model/Image.java
@@ -0,0 +1,28 @@
+package com.personalProject.basicshop.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Blob;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Image {
+    private Long id;
+    private String fileName;
+    private String fileType;
+    private Blob image;
+    private String downloadUrl;
+
+    @ManyToOne
+    @JoinColumn(name ="product_id")
+    private Product product;
+}
